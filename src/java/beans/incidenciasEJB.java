5a1ca5fbf4f9892834c5f4be89b334d01d79ca1d
Angelo <anjodanan@hotmail.com>
2018-03-20 11:37:41 +0100

    Actualizado JPA

---
+++ src/java/beans/incidenciasEJB.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans;
+
+import entities.Empleado;
+import exceptions.MisExcepciones;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceUnit;
+
+/**
+ *
+ * @author DAM
+ */
+@Stateless
+public class incidenciasEJB {
+
+    @PersistenceUnit EntityManagerFactory emf;
+    
+    public void altaEmpleado(Empleado e) throws MisExcepciones {
+        EntityManager em = emf.createEntityManager();
+        // Comprobamos si ya existe un cocinero 
+        Empleado aux = em.find(Empleados.class, e.getNombre());
+        if (aux != null) {
+            em.close();
+            throw new MisExcepciones("Ya existe un cocinero con ese nombre");
+        }
+        em.persist(e);
+        em.close();
+    }
+}
